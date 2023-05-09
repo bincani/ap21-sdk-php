4c2f476bebd13bc745206130cd23885da6c7a55d
ben.incani <ben.incani@factoryx.com.au>
2023-05-09 12:18:22 +1000

    update libs

--- lib/Log.php
+++ lib/Log.php
@@ -98,35 +98,35 @@ class Log {
     }
 
     public static function debug($message, array $context = []) {
-        self::getLogger()->addDebug($message, $context);
+        self::getLogger()->debug($message, $context);
     }
 
     public static function info($message, array $context = []) {
-        self::getLogger()->addInfo($message, $context);
+        self::getLogger()->info($message, $context);
     }
 
     public static function notice($message, array $context = []) {
-        self::getLogger()->addNotice($message, $context);
+        self::getLogger()->notice($message, $context);
     }
 
     public static function warning($message, array $context = []) {
-        self::getLogger()->addWarning($message, $context);
+        self::getLogger()->warning($message, $context);
     }
 
     public static function error($message, array $context = []) {
-        self::getLogger()->addError($message, $context);
+        self::getLogger()->error($message, $context);
     }
 
     public static function critical($message, array $context = []) {
-        self::getLogger()->addCritical($message, $context);
+        self::getLogger()->critical($message, $context);
     }
 
     public static function alert($message, array $context = []) {
-        self::getLogger()->addAlert($message, $context);
+        self::getLogger()->alert($message, $context);
     }
 
     public static function emergency($message, array $context = []) {
-        self::getLogger()->addEmergency($message, $context);
+        self::getLogger()->emergency($message, $context);
     }
 
 }
