7f2d79b7f96473aea4e2d914648d1fdc6f9dd857
ben.incani <ben.incani@factoryx.com.au>
2023-05-16 15:47:33 +1000

    fix freestock

---
+++ lib/Product/FuturePrice.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * class FuturePrice
+ */
+
+namespace PHPAP21\Product;
+
+use PHPAP21\Product as Product;
+
+class FuturePrice extends Product
+{
+    protected $resourceKey = 'FuturePrice';
+}
