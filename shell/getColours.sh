096633de2cbe7178858db3a5bce17f7882c78afe
ben.incani <ben.incani@factoryx.com.au>
2024-11-21 16:50:59 +1100

    time request

--- shell/getColours.sh
+++ shell/getColours.sh
@@ -10,8 +10,14 @@ fi
 # Colours?countryCode={countryCode}&query={queryString}
 # Query - Optional - Sequence and Product Reference Type. Up to 5 combinations can be entered.
 
+start=`date +%s`
 # GET ALL colours
 curl --insecure --silent \
   -X GET "$ApiUrl/Colours?CountryCode=$CountryCode" \
   -u "$ApiUser:$ApiPassword" \
-  -H "Content-Type: application/xml" > colours.xml &
+  -H "Content-Type: application/xml" > colours.xml
+
+end=`date +%s`
+runtime=$((end-start))
+echo "runtime=$runtime secs"
+
