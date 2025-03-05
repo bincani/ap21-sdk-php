7c829363921d97f1e67c2de11c18eec1eb32e0c6
ben.incani <ben.incani@factoryx.com.au>
2025-03-05 11:12:33 +1100

    get orders

--- lib/Order.php
+++ lib/Order.php
@@ -5,28 +5,15 @@
 
 namespace PHPAP21;
 
+use PHPAP21\Person\Orders;
+
 /**
  * @method array close() Close an Order
  * @method array open() Re-open a closed Order
  * @method array cancel(array $data) Cancel an Order
  *
  */
-class Order extends HTTPXMLResource
+class Order extends Orders
 {
-    protected $resourceKey = 'order';
 
-    /**
-     * @inheritDoc
-     */
-    protected $childResource = array (
-    );
-
-    /**
-     * @inheritDoc
-     */
-    protected $customPostActions = array(
-        'close',
-        'open',
-        'cancel',
-    );
 }
